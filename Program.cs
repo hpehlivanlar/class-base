b8016bd7baec776cad835c2d84368b565310db18
Hasan PEHLİVANLAR <hpehlivanlar@gmail.com>
2019-08-06 23:32:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿//#define Kullanim1
+#define Kullanim2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_base_kullanımı
+{
+
+
+#if (Kullanim1)
+
+
+
+    class Kitap
+    {
+        public void Ad()
+        {
+            Console.WriteLine("kitap adı");
+        }
+
+    }
+
+    class Roman:Kitap
+    {
+        public void IsnbNO() 
+        {
+            base.Ad();
+            Console.WriteLine("Isbo no");
+
+             
+
+        }
+    }
+
+    class RomanDetay:Roman
+    {
+        public void SayfaNo()
+        {
+            base.IsnbNO();
+            Console.WriteLine("sayfa no");
+
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RomanDetay romanDetay = new RomanDetay();
+            romanDetay.SayfaNo();
+
+            //Roman roman = new Roman();
+            //roman.IsnbNO();
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+
+
+
+#elif (Kullanim2)
+
+    class Kitap
+    {
+        public Kitap()
+        {
+            Console.WriteLine("kitap adı:OD");
+        }
+
+        public Kitap(string adi)
+        {
+            Console.WriteLine("kitap adı"+adi);
+        }
+
+
+
+
+    }
+
+    class Roman : Kitap
+    {
+
+        public Roman() :base ()
+        {
+            Console.WriteLine("ISBN  No :123");
+        }
+
+        public Roman(string isbnno) :base (isbnno)
+        {
+            Console.WriteLine("isbenno adı" + isbnno);
+
+        }
+
+    }
+
+
+   
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+            Roman roman = new Roman();
+
+           //Roman roman1 = new Roman("132");
+
+
+
+
+
+
+
+
+                Console.ReadKey();
+
+
+
+            }
+        }
+
+
+#endif
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("class-base-kullanımı")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("class-base-kullanımı")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("3114bc5c-4808-4f40-b992-ad667c8b5f32")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
